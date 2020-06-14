41846468f1db84f349ecc9791cda80a8406b4eb8
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-14 12:12:22 +0530

    defining function with never return type

---
+++ script_8.ts
@@ -0,0 +1,14 @@
+function error(message: string): never
+{
+  throw new Error(message);
+}
+
+function fail() {
+  return error("Something failed");
+}
+
+function infiniteLoop(): never
+{
+  while (true)
+  {}
+}
