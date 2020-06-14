8e5ce12c0d1eb74166365f9d7fe3c68710aca1dd
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-14 11:23:27 +0530

    script to create multi-line formatted string

--- script_4.ts
+++ script_4.ts
@@ -6,3 +6,11 @@ let binary: number = 0b1111;
 let octal: number = 0o443;
 
 let color: string = "red";
+
+let fullName: string = `Bob Borington`;
+let age: number = 34;
+
+let sentence: string = `Hello, my name is ${fullName}.
+
+I'll be ${age + 1} years old.`;
+console.log(sentence);
