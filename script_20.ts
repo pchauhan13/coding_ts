bc83747ea0453733e04f54dbcb54fe681d0923da
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-18 16:30:54 +0530

    script to extend multiple interfaces using one interface

--- script_20.ts
+++ script_20.ts
@@ -2,11 +2,16 @@ interface Shape {
   color: string;
 }
 
-interface Square extends Shape {
+interface PenStroke {
+  penWidth: number;
+}
+
+interface Square extends Shape, PenStroke {
   sideLength: number;
 }
 
 let square = {} as Square;
 square.color = "Blue";
 square.sideLength = 3;
+square.penWidth = 4.0;
 console.log(square);
