0f3b9cce699f5070db06c91c52012982298455bb
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-18 16:18:49 +0530

    script to implement an interface using class

---
+++ script_18.ts
@@ -0,0 +1,17 @@
+interface ClockInterface {
+  currentTime: Date;
+}
+
+class Clock implements ClockInterface
+{
+  currentTime: Date = new Date();
+  constructor(h: number, m: number)
+  {
+
+  }
+
+  setTime(time: Date)
+  {
+    this.currentTime = time;
+  }
+}
