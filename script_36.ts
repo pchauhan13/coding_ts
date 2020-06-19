2e76daf3a25e7f2c424fcefebdcfb256bc706d5e
Prashant Singh Chauhan <chauhanprashant480@gmail.com>
2020-06-19 18:55:27 +0530

    script to create a generic type class and using it to create objects

---
+++ script_36.ts
@@ -0,0 +1,12 @@
+class GenericNumber<T> {
+  zeroValue: T;
+  add: (x: T, y: T) => T;
+}
+
+let myGenericNumber = new GenericNumber<number>();
+myGenericNumber.zeroValue = 0;
+myGenericNumber.add = function (x, y) { return x + y; };
+
+let myGenericString = new GenericNumber<string>();
+myGenericString.zeroValue = "";
+myGenericString.add = function (x, y) { return x + y; };
